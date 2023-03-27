7e47aaf583606d1c1d8dbd6abce64d0da52713d2
guacajoely <guacajoely@gmail.com>
2023-03-27 11:50:55 -0400

    complete

--- scripts/carDealership.js
+++ scripts/carDealership.js
@@ -1,6 +1,6 @@
 import { sales } from "./data.js";
 
-//first off, how long is this freaking array...
+//first off, how long is this array of sales...
 console.log(sales.length)
 
 //1. Total profit for 2017
@@ -19,19 +19,31 @@ const totalProfit = arrayOfProfits.reduce((a, b) => {return a + b}).toFixed(2)
 
 export const profit = `<h3>1. Total profit for 2017 = $<span style="font-size: 25px">${totalProfit}</span></h3>`
 
+
+
+
+
 //2. In which month did they sell the most cars?
 //filter array by month to get all sales from each month organized
 // January = sales[0]  February = sales[1]  etc...
-const arrayOfAllSalesByMonth = []
+const unsortedSalesByMonth = []
 for(let i = 1; i < 13; i++){
     const arrayOfSingleMonthsSales = sales.filter(sale => {
+        if(i<10){
         return sale.purchase_date.startsWith(`2017-0${i}`)
+        }
+        else{
+            return sale.purchase_date.startsWith(`2017-${i}`)
+            }
     })
-    arrayOfAllSalesByMonth.push(arrayOfSingleMonthsSales)
+    unsortedSalesByMonth.push(arrayOfSingleMonthsSales)
 }
 
+console.log(unsortedSalesByMonth)
+const copyOfUnsortedSalesByMonth = [...unsortedSalesByMonth]
+
 //sort the array of sales for each month by length
-const monthlySalesByLength = arrayOfAllSalesByMonth.sort((a, b) => { return b.length - a.length})
+const monthlySalesByLength = copyOfUnsortedSalesByMonth.sort((a, b) => { return b.length - a.length})
 
 //grab the month number from the first array
 const [,highestSalesMonth] = monthlySalesByLength[0][0].purchase_date.split('-')
@@ -44,6 +56,10 @@ function getMonthName(monthNumber) {
   }
 
   export const monthMostSales = `<h3>2. Month with the highest sales = <span style="font-size: 25px">${getMonthName(highestSalesMonth)}</span></h3>`
+
+
+
+
 
 //3. Which salesperson sold the most cars?
 
@@ -71,17 +87,19 @@ const singleCounts = Object.values(emailsWithCounts.reduce((acc,{email,count})=>
 const sortedByCount = singleCounts.sort((a, b) => { return b.count - a.count})
 const emailWithMostSales = sortedByCount[0].email
 
-console.log(emailWithMostSales)
 
 //we got their email, now lets grab the whole object to get their name
 const objectWithMostSales = sales.find((sale) => {
     return sale.sales_agent.email === emailWithMostSales
 })
 
-const firstName = objectWithMostSales.sales_agent.first_name
-const lastName = objectWithMostSales.sales_agent.last_name
+const firstNameSales = objectWithMostSales.sales_agent.first_name
+const lastNameSales = objectWithMostSales.sales_agent.last_name
 
-export const mostCarsSold = `<h3>3. Saleperson that sold the most cars = <span style="font-size: 20px">${firstName} ${lastName} (${emailWithMostSales})</span></h3>`
+export const mostCarsSold = `<h3>3. Saleperson that sold the most cars = <span style="font-size: 25px">${firstNameSales} ${lastNameSales} (${emailWithMostSales})</span></h3>`
+
+
+
 
 
 //4. Which salesperson made the most profit?
@@ -110,8 +128,6 @@ for(const email of singleEmails){
 const sorted = emailsWithProfits.sort((a, b) => { return b.totalProfit - a.totalProfit})
 const emailWithMostProfit = sorted[0].email
 
-console.log(emailWithMostProfit)
-
 //we got their email, now lets grab the whole object to get their name
 const objectWithMostProfit = sales.find((sale) => {
     return sale.sales_agent.email === emailWithMostProfit
@@ -122,11 +138,104 @@ const lastNameProfit = objectWithMostProfit.sales_agent.last_name
 
 export const personMostProfit = `<h3>4. Saleperson that made the most profit = <span style="font-size: 25px"> ${firstNameProfit} ${lastNameProfit} (${emailWithMostProfit}) </span></h3>`
 
+
+
+
+
 //5. Which model was the most popular?
-export const mostPopularModel = `<h3>5. Most popular car model sold = <span style="font-size: 25px"> INSERT RESULT HERE </span></h3>`
+//first lets grab just the model from each sale
+const arrayOfModels = sales.map( sale => {
+    return sale.vehicle.model
+})
+
+// now count how many times that model pops up throughout the array and create a new array of the emails with counts
+const modelsWithCounts = []
+for(const model of arrayOfModels){
+    var count = 0;
+    for(const anotherModel of arrayOfModels){
+        if(model === anotherModel){count++}
+    }
+    modelsWithCounts.push({model, count})
+}
+
+//we only need one count per model
+const singleCountPerModel = Object.values(modelsWithCounts.reduce((acc,{model,count})=>
+   ((acc[model] = acc[model] || {model, count: 0}).count = count, acc), {}));
+
+//now sort them most to fewest and spit out array[0]
+const modelsSortedByCount = singleCountPerModel.sort((a, b) => { return b.count - a.count})
+const modelWithMostSales = modelsSortedByCount[0]
+
+export const mostPopularModel = `<h3>5. Most popular car model sold = <span style="font-size: 25px"> ${modelWithMostSales.model} (${modelWithMostSales.count} sold)</span></h3>`
+
+
+
+
 
 //6. Which bank provided the most loans to our customers?
-export const mostLoans = `<h3>6. Bank that provided the most loans = <span style="font-size: 25px"> INSERT RESULT HERE </span></h3>`
+//this is going to be very similar to the previous, (almost identical) but replace model with bank name on the "credit_provider" attribute under credit
+//first lets grab just the bank name from each sale
+
+const arrayOfBanks = sales.map( sale => {
+    return sale.credit.credit_provider
+})
+
+// now count how many times that bank pops up throughout the array and create a new array of the banks with counts
+const banksWithCounts = []
+for(const bank of arrayOfBanks){
+    var count = 0;
+    for(const anotherBank of arrayOfBanks){
+        if(bank === anotherBank){count++}
+    }
+    banksWithCounts.push({bank, count})
+}
+
+//we only need one count per bank
+const singleCountPerBank = Object.values(banksWithCounts.reduce((acc,{bank,count})=>
+   ((acc[bank] = acc[bank] || {bank, count: 0}).count = count, acc), {}));
+
+//now sort them most to fewest and spit out array[0]
+const banksSortedByCount = singleCountPerBank.sort((a, b) => { return b.count - a.count})
+const bankWithMostSales = banksSortedByCount[0]
+
+export const mostLoans = `<h3>6. Bank that provided the most loans = <span style="font-size: 25px"> ${bankWithMostSales.bank} (${bankWithMostSales.count} loans issued)</span></h3>`
+
+
+
+
 
 //7. In which month did zero cars get sold?
-export const zeroSales = `<h3>7. Month with zero sales = <span style="font-size: 25px"> INSERT RESULT HERE </span></h3>`
+// get all the sales that match each month into their own array
+// January = salesByMonth[0]  February = salesByMonth[1]  etc...
+const salesByMonth = []
+for(let i = 1; i < 13; i++){
+    const arrayOfSingleMonthsSales = []
+    for(const sale of sales){
+        const splitSaleDate = sale.purchase_date.split('-')
+        if(i<10 && splitSaleDate[1] == `0${i}`){
+            arrayOfSingleMonthsSales.push(sale) 
+        }
+        else if(i>10 && splitSaleDate[1] == `${i}`){
+            arrayOfSingleMonthsSales.push(sale) 
+        }
+    }
+    salesByMonth.push(arrayOfSingleMonthsSales)
+}
+console.log(salesByMonth)
+
+//now get the name of the month with 0 sales
+//grab the month number from the first array
+let monthWithZeroSales = null
+salesByMonth.forEach(month => {
+    if(month.length === 0){
+        const monthNumber = salesByMonth.indexOf(month) + 1
+        monthWithZeroSales = monthNumber
+    }
+})
+
+console.log(monthWithZeroSales)
+
+//we can just use the funtion we used in exercise 2 to get the month name
+
+
+export const zeroSales = `<h3>7. Month with zero sales = <span style="font-size: 25px"> ${getMonthName(monthWithZeroSales)} </span></h3>`
